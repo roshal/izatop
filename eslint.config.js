c9c61e5ee48799bd69b09e4dedc34dfb63899d15
roshal <git@roshal.online>
2025-02-24 05:18:19 +0300

    polish

--- eslint.config.js
+++ eslint.config.js
@@ -6,15 +6,20 @@ const globals = require('globals')
 const js = require('@eslint/js')
 const stylistic = require('@stylistic/eslint-plugin')
 
-/** @typedef {
+/**
+@typedef Config
+@type {
 	import('eslint').Linter.Config
-} Config */
+}
+*/
 
 const eslintFlatCompat = new FlatCompat()
 
-/** @type {
+/**
+@type {
 	Array<Config>
-} */
+}
+*/
 const eslintConfigArray = module.exports = []
 
 void eslintConfigArray.push({
